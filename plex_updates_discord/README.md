982614f41b383a6450bde710a69bf8246a50bf27
samwiseg00 <samwiseg00@ReverseFlash.local>
2018-07-12 23:24:08 -0400

    Fixed the script (last.json broke). Added comments.

--- plex_updates_discord/README.md
+++ plex_updates_discord/README.md
@@ -1,8 +1,12 @@
 ## Plex Client/Server Updates to Discord
-I reworked the script so it's all in one. It has also been updated to work on the new Discourse Plex Forums.
+I reworked the script so it's all in one. It has also been updated to work on the new Discourse Plex Forums. The first run will now cache the last post and no notification will be sent.
 
 The new script only works with discord. I removed slack compatibility.
+
+use python 3.6+
 
 Example:
 
 <img width="450" alt="Example" src="https://i.imgur.com/DpuzHZL.png">
+
+Thanks to @JonnyWong16 for some of the code optimizations
--- plex_updates_discord/plex_client_update_discord.py
+++ plex_updates_discord/plex_client_update_discord.py
@@ -3,12 +3,11 @@ import requests
 import sys
 import time
 from random import randint
-from datetime import datetime, date
 import tomd
 import re
+import html
 
 #################### EDIT ####################
-
 
 discord_user = "User"
 discord_url = "https://discordapp.com/api/webhooks/XXXXXXXXXXX/XXXXXXXXX"
@@ -16,104 +15,140 @@ discord_headers = {'content-type': 'application/json'}
 
 # Comment out lines of devices you do not want to be notified about using --> #
 device_list =   [
-    (178323, "Plex for Alexa", "#31C4F3","https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/alexa.png" ),
-    (29115, "Plex for Android", "#a4ca39", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/android.png"),
-    (121357, "Plex for Apple TV", "#858487", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/apple.png"),
-    (143389, "Plex for Chromecast", "#3cba54","https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/chrome.png"),
-    (31524, "Plex for iOS", "#858487", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/apple.png"),
-    (213558, "Plex for Kodi (add-on)", "#00f2ff","https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/kodi.png"),
-    (120475, "Plex Media Player", "#e67817","https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/plex.png"),
-    (90217, "Plex for PlayStation 3 & PlayStation 4", "#003791", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/ps.png"),
-    (9463, "Plex for Roku", "#6d3c97", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/roku.png"),
-    (224615, "Plex for Samsung (2016+ televisions)", "#034ea2", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/samsung.png"),
-    (86563, "Plex for Smart TVs & TiVo", "#e5a00d", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/tivo.png"),
-    (228282, "Plex for Sonos", "#ffffff", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/sonos.png"),
-    (223132, "Plex VR", "#e67817", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/plex.png"),
-    (20528, "Plex Web", "#e67817", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/plex.png"),
-    (85265, "Plex for Xbox One", "#107c10", "https://raw.githubusercontent.com/samwiseg00/plex/master/plex_updates_discord/images/xbox.png")
+    (178323, "Plex for Alexa", "#31C4F3","http://tny.im/eGj"),
+    (29115, "Plex for Android", "#a4ca39", "http://tny.im/eVk"),
+    (121357, "Plex for Apple TV", "#858487", "http://tny.im/eVl"),
+    (143389, "Plex for Chromecast", "#3cba54","http://tny.im/eVm"),
+    (31524, "Plex for iOS", "#858487", "http://tny.im/eVl"),
+    (213558, "Plex for Kodi (add-on)", "#00f2ff","http://tny.im/eVn"),
+    (120475, "Plex Media Player", "#e67817","http://tny.im/eVo"),
+    (90217, "Plex for PlayStation 3 & PlayStation 4", "#003791", "http://tny.im/eVp"),
+    (9463, "Plex for Roku", "#6d3c97", "http://tny.im/eVq"),
+    (224615, "Plex for Samsung (2016+ televisions)", "#034ea2", "http://tny.im/eVr"),
+    (86563, "Plex for Smart TVs & TiVo", "#e5a00d", "http://tny.im/eVs"),
+    (228282, "Plex for Sonos", "#ffffff", "http://tny.im/eVt"),
+    (223132, "Plex VR", "#e67817", "http://tny.im/eVo"),
+    (20528, "Plex Web", "#e67817", "http://tny.im/eVo"),
+    (85265, "Plex for Xbox One", "#107c10", "http://tny.im/eVu")
     ]
 
 #################### DO NOT EDIT BELOW THIS LINE ####################
 
 def cleanhtml(text):
-  cleanr = re.compile('<.*?>')
-  cleantext = re.sub(cleanr, '', text)
-  return cleantext
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', text)
+    return cleantext
+
+def get_latest_post(category_id):
+    get_updates = requests.get("https://forums.plex.tv/t/{}.json".format(category_id)).json()
+    first_run_post_count = get_updates.get('highest_post_number', get_updates.get('timeline_lookup', [[None]])[-1][0])
+    return first_run_post_count
 
 for discourseID, deviceType, message_color, thumbnail in device_list:
+    #Sleep for a bit so we dont get soft banned
     rand_sleep = (randint(5,10))
     print ("Sleeping for {} seconds...".format(rand_sleep))
     time.sleep (rand_sleep)
 
+    #Convert hex color to integer
     message_color = message_color.lstrip('#')
     message_color = int(message_color, 16)
 
-    current_time = datetime.now().strftime("%B %d, %Y %I:%M:%S %p %Z")
-
+    #Check to see if the cache file exsists
     if not os.path.exists("/tmp/plex_{}_last_post.txt".format(discourseID)):
         with open("/tmp/plex_{}_last_post.txt".format(discourseID), 'w'): pass
 
-    get_updates = requests.get("https://forums.plex.tv/t/{}/last.json".format(discourseID)).json()['post_stream']['posts']
+    try:
+        #Try to read the cache file
+        prev_comment_file_read = open("/tmp/plex_{}_last_post.txt".format(discourseID),"r")
+        prev_comment = int(prev_comment_file_read.read())
+        prev_comment_file_read.close()
+
+    except ValueError:
+        #If the cache file does not exsist create it with the latest post number
+        prev_comment_file = open("/tmp/plex_{}_last_post.txt".format(discourseID),"w+")
+        prev_comment_file.write('{}'.format(get_latest_post(discourseID)))
+        prev_comment_file.close()
+
+    #Pull the integer stored in the cache file
+    prev_comment_file_read = open("/tmp/plex_{}_last_post.txt".format(discourseID),"r")
+    prev_comment = int(prev_comment_file_read.read())
+    prev_comment_file_read.close()
+
+    #Get the laest json data for the post
+    get_updates = requests.get("https://forums.plex.tv/t/{}/{}.json".format(discourseID, prev_comment)).json()['post_stream']['posts']
 
     posts = {d['post_number']: d for d in get_updates}
 
     last_post = max(posts)
 
+    #Pull the summary and convert it to markdown
     summary = tomd.convert(posts[last_post]['cooked'])
-    summary = cleanhtml(summary).replace("&quot;", "\"").replace("&amp;", "&").replace("&apos;", "\'").replace("&#39;", "\'")
-    summary = (summary[:2045] + '...') if len(summary) > 2045 else summary
+
+    #Unescape the summary and remove the HTML
+    summary = html.unescape(cleanhtml(summary))
+
+    #Limit the sumary length so it fits in a discord message
+    summary_limit = summary[:2045] + (summary[2045:] and '...')
+
     author_username = posts[last_post]['username']
+
     created = posts[last_post]['created_at']
+
     avatar_template = posts[last_post]['avatar_template']
+
     author_icon = posts[last_post]['avatar_template'].replace("{size}", "48")
 
     if len(summary) < 4:
+        #If the post is too short lets skip it
         print ("The post length for {} is less than 4 characters... skipping".format(deviceType))
         break
+
     else:
         pass
 
-    version = ""
+    #Empty the version var
+    version = ''
 
+    #Regex to pull a systematic version number
     version_re = re.compile('(\d+\.)(\d+\.)(\d+)')
     version_search = version_re.search(summary)
+
     if version_search:
         version = version_search.group()
+
     else:
         try:
+            #Regex to pull a 2 dot version number
             version_re = re.compile('(\d+\.)(\d+)')
             version_search = version_re.search(summary)
             version = version_search.group()
+
         except:
             pass
 
+    #Format the version number if we found one
     if version:
         version = ("v{}".format(version))
 
+    #Add the full path URL if its misssing
     if "http" not in author_icon:
         author_icon = "https://forums.plex.tv{}".format(author_icon)
+
     else:
         pass
 
-    try:
-        prev_comment_file_read = open("/tmp/plex_{}_last_post.txt".format(discourseID),"r")
-        prev_comment = int(prev_comment_file_read.read())
-        prev_comment_file_read.close()
-    except ValueError:
-        prev_comment_file = open("/tmp/plex_{}_last_post.txt".format(discourseID),"w+")
-        prev_comment_file.write("1")
-        prev_comment_file.close()
-
-    prev_comment_file_read = open("/tmp/plex_{}_last_post.txt".format(discourseID),"r")
-    prev_comment = int(prev_comment_file_read.read())
-    prev_comment_file_read.close()
-
+    #Check to see if the post number is the same as the cached one
     if prev_comment == last_post:
         print ("{} version remains unchanged... exiting".format(deviceType))
+
     else:
+        #If not lets write it to the cache file
         prev_comment_file = open("/tmp/plex_{}_last_post.txt".format(discourseID),"w+")
         prev_comment_file.write("{}".format(last_post))
         prev_comment_file.close()
+
+        #Compose the json message we will send to discord
         message = {
             "username": discord_user,
             "content": "New Version - {} {}".format(deviceType, version),
@@ -123,19 +158,19 @@ for discourseID, deviceType, message_color, thumbnail in device_list:
                      "url": thumbnail
                      },
                 "author": {
-                     "name": author_username,
-                     "url": "https://forums.plex.tv/t/{}/{}".format(discourseID, last_post),
+                     "name": "Post by {}".format(author_username),
+                     "url": "https://forums.plex.tv/u/{}/summary".format(author_username),
                      "icon_url": author_icon
                      },
                 "title": "New Version - {} {}".format(deviceType, version),
                 "color": message_color,
                 "url": "https://forums.plex.tv/t/{}/{}".format(discourseID, last_post),
-                "description": summary,
+                "description": summary_limit,
                 "timestamp": created
                     }
             ]
         }
-        # Send notification
+        # Send the notification to Discord
         r = requests.post(discord_url, headers=discord_headers, json=message)
         print (r.content)
         print ("Discord notification sent for device {}!".format(deviceType))
