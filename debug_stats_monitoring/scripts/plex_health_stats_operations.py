f3fd7dc887268668bfb6b618b6df74a011b99b8b
samwiseg00 <samwiseg0@none.com>
2018-08-08 15:03:41 -0400

    Add --plex_conversion_queue to the healh stats script

--- debug_stats_monitoring/scripts/plex_health_stats_operations.py
+++ debug_stats_monitoring/scripts/plex_health_stats_operations.py
@@ -2,17 +2,20 @@
 
 import os
 import requests
+import xmltodict
 import json
 import sys
 import argparse
 from argparse import RawTextHelpFormatter
 import script_config
 
+
 def get_activity(key):
     payload = {'apikey': '{}'.format(script_config.tau_api_key), 'cmd': 'get_activity'}
     get_tau_activity = requests.get('{}/api/v2'.format(script_config.tau_url), params=payload).json()['response']['data']
     count = get_tau_activity['{}'.format(key)]
     return count
+
 
 def get_web_threads(type):
     get_plex_threads = requests.get('{}/connections?X-Plex-Token={}'.format(script_config.plex_url, script_config.plex_token))
@@ -22,17 +25,20 @@ def get_web_threads(type):
         output = (get_plex_threads.text)
     return output
 
+
 def count_lines(file):
     with open(file) as websoc_log:
         for i, l in enumerate(websoc_log):
             pass
     return i + 1
 
+
 def web_soc_search(search_string, file):
     with open(file, 'r') as searchfile:
         for line in searchfile:
             if search_string in line:
                 print (line)
+
 
 def log_error_count(file):
     count = 0
@@ -42,6 +48,7 @@ def log_error_count(file):
                 count = count + 1
         return count
 
+
 def get_folder_size(start_path):
     total_size = 0
     for dirpath, dirnames, filenames in os.walk(start_path):
@@ -50,9 +57,19 @@ def get_folder_size(start_path):
             total_size += os.path.getsize(fp)
     return total_size
 
+
+def get_conversion_queue():
+    get_conversion_queue = requests.get('{}/status/sessions/background?X-Plex-Token={}'.format(script_config.plex_url, script_config.plex_token))
+    conversion_queue = json.loads(json.dumps(xmltodict.parse(get_conversion_queue.content)))
+    for items in conversion_queue.keys():
+        items_converting = conversion_queue[items]['@size']
+    return items_converting
+
+
 STREAM_SELECTOR = ['total', 'direct_stream', 'direct_play' , 'transcode']
 
 WEBTHREAD_SELECTOR = ['count', 'dump']
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='Plex health/stats operations',
@@ -79,13 +96,16 @@ if __name__ == "__main__":
     parser.add_argument("--plex_server_log",  action='store_true',
         help='Use plex server log ie. Plex Media Server.log')
 
+    parser.add_argument("--plex_conversion_queue",  action='store_true',
+        help='Check the Plex conversion queue')
+
     parser.add_argument("--location", type=str,
         help='Location of a log file or folder')
 
     parser.add_argument("--dummy",  action='store_true',
         help='Used as a placeholder to work around a zabbix limitation. This does nothing!')
 
-    parser.add_argument('--version', action='version', version='%(prog)s v0.2',
+    parser.add_argument('--version', action='version', version='%(prog)s v0.3',
         help='Print version and exit.')
 
     opts = parser.parse_args()
@@ -126,10 +146,13 @@ if __name__ == "__main__":
             print (log_error_count('{}Plex Media Server.log'.format(script_config.plex_log_location)))
 
         elif not opts.location:
-            print('ERROR: --file argument or --plex_server_log must be supplied')
+            print('ERROR: --location argument or --plex_server_log must be supplied')
 
         else:
             print (log_error_count(opts.location))
+
+    elif opts.plex_conversion_queue:
+        print (get_conversion_queue())
 
     elif opts.get_folder_size:
         if not opts.location:
